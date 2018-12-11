34fc03da53b38234f5a7738f796969c2098976ef
unknown <d.ponikowski11@gmail.com>
2018-12-11 21:39:34 +0100

    funkcja xml_data_frame

--- kawa.R
+++ kawa.R
@@ -1,58 +1,33 @@
 library(XML)
 options(stringsAsFactors = FALSE)
-#³adanowanie danych pe³nych
-Badges <- data.frame(t(data.frame(xmlToList("Badges.xml"))),row.names = 1:8031)
-Comments <- data.frame(t(data.frame(xmlToList("Comments.xml"))),row.names = 1:3593)
-
-#³adowanie danych z brakiem niektorych zmiennych (uzupe³nienie ich NA)
 
 changes_names <- function(x,nazwy){
   names(x) <- nazwy
   x
 }
 
-PostHistory <- xmlToList("PostHistory.xml")
-nazwy <- names(PostHistory[[19]])
-PostHistory <- lapply(PostHistory,"[",nazwy)
-PostHistory <- lapply(PostHistory, changes_names,nazwy)
-PostHistory <- data.frame(t(data.frame(PostHistory,check.names = FALSE)))
-rownames(PostHistory) <- 1:nrow(PostHistory)
+xml_data_frame <- function(xml){
+  df <- xmlToList(xml)
+  len <- lapply(df,length)
+  n <- which(unlist(len) == max(unlist(len)))[1]          
+  nazwy <- names(df[[n]])
+  df <- lapply(df,"[",nazwy)
+  df <- lapply(df, changes_names,nazwy)
+  df <- data.frame(t(data.frame(df,check.names = FALSE)))
+  rownames(df) <- 1:nrow(df)
+  df
+}
 
-Posts <- xmlToList("Posts.xml")
-len <- lapply(Posts,length)
-n <- which(unlist(len) == max(unlist(len)))[1]          
-nazwy <- names(Posts[[n]])
-Posts <- lapply(Posts,"[",nazwy)
-Posts <- lapply(Posts, changes_names,nazwy)
-Posts <- data.frame(t(data.frame(Posts,check.names = FALSE)))
-rownames(Posts) <- 1:nrow(Posts)
+Comments <- xml_data_frame("Comments.xml")
 
+Badges <- xml_data_frame("Badges.xml")
 
-Users <- xmlToList("Users.xml")
-len <- lapply(Users,length)
-n <- which(unlist(len) == max(unlist(len)))[1]          
-nazwy <- names(Users[[n]])
-Users <- lapply(Users,"[",nazwy)
-Users <- lapply(Users, changes_names,nazwy)
-Users <- data.frame(t(data.frame(Users,check.names = FALSE)))
-rownames(Users) <- 1:nrow(Users)
+PostsHistory <- xml_data_frame("PostHistory.xml")
 
+Posts <- xml_data_frame("Posts.xml")
 
-Tags <- xmlToList("Tags.xml")
-len <- lapply(Tags,length)
-n <- which(unlist(len) == max(unlist(len)))[1]          
-nazwy <- names(Tags[[n]])
-Tags <- lapply(Tags,"[",nazwy)
-Tags <- lapply(Tags, changes_names,nazwy)
-Tags <- data.frame(t(data.frame(Tags,check.names = FALSE)))
-rownames(Tags) <- 1:nrow(Tags)
+Users <- xml_data_frame("Users.xml")
 
+Tags <- xml_data_frame("Tags.xml")
 
-Votes <- xmlToList("Votes.xml")
-len <- lapply(Votes,length)
-n <- which(unlist(len) == max(unlist(len)))[1]          
-nazwy <- names(Votes[[n]])
-Votes <- lapply(Votes,"[",nazwy)
-Votes <- lapply(Votes, changes_names,nazwy)
-Votes <- data.frame(t(data.frame(Votes,check.names = FALSE)))
-rownames(Votes) <- 1:nrow(Votes)
+Votes <- xml_data_frame("Votes.xml")
