a9ae2da353b764eabcaa457eaad55b929f0c5475
unknown <d.ponikowski11@gmail.com>
2018-12-11 16:33:50 +0100

    Wczytanie danych, uzupełnienie braków NA

--- kawa.R
+++ kawa.R
@@ -1,10 +1,58 @@
 library(XML)
 options(stringsAsFactors = FALSE)
+#³adanowanie danych pe³nych
 Badges <- data.frame(t(data.frame(xmlToList("Badges.xml"))),row.names = 1:8031)
 Comments <- data.frame(t(data.frame(xmlToList("Comments.xml"))),row.names = 1:3593)
-PostHistory <- data.frame(t(data.frame(lapply(xmlToList("PostHistory.xml"),"[",1:6))))
-Posts <- data.frame(t(data.frame(lapply(xmlToList("Posts.xml"), "[", 1:9))))
-Users <- data.frame(t(data.frame(lapply(xmlToList("Users.xml"),"[",1:9))),row.names = 1:5826)
-Tags <- data.frame(t(data.frame(lapply(xmlToList("Tags.xml"),"[",1:3))),row.names = 1:106)
-Votes <- data.frame(t(data.frame(lapply(xmlToList("Votes.xml"),"[",1:4))))
-#wczytanie danych, nie wiem jak wczytaæ do data.frame z listy gdy jest ró¿na d³ugoœæ elementów w liœcie
+
+#³adowanie danych z brakiem niektorych zmiennych (uzupe³nienie ich NA)
+
+changes_names <- function(x,nazwy){
+  names(x) <- nazwy
+  x
+}
+
+PostHistory <- xmlToList("PostHistory.xml")
+nazwy <- names(PostHistory[[19]])
+PostHistory <- lapply(PostHistory,"[",nazwy)
+PostHistory <- lapply(PostHistory, changes_names,nazwy)
+PostHistory <- data.frame(t(data.frame(PostHistory,check.names = FALSE)))
+rownames(PostHistory) <- 1:nrow(PostHistory)
+
+Posts <- xmlToList("Posts.xml")
+len <- lapply(Posts,length)
+n <- which(unlist(len) == max(unlist(len)))[1]          
+nazwy <- names(Posts[[n]])
+Posts <- lapply(Posts,"[",nazwy)
+Posts <- lapply(Posts, changes_names,nazwy)
+Posts <- data.frame(t(data.frame(Posts,check.names = FALSE)))
+rownames(Posts) <- 1:nrow(Posts)
+
+
+Users <- xmlToList("Users.xml")
+len <- lapply(Users,length)
+n <- which(unlist(len) == max(unlist(len)))[1]          
+nazwy <- names(Users[[n]])
+Users <- lapply(Users,"[",nazwy)
+Users <- lapply(Users, changes_names,nazwy)
+Users <- data.frame(t(data.frame(Users,check.names = FALSE)))
+rownames(Users) <- 1:nrow(Users)
+
+
+Tags <- xmlToList("Tags.xml")
+len <- lapply(Tags,length)
+n <- which(unlist(len) == max(unlist(len)))[1]          
+nazwy <- names(Tags[[n]])
+Tags <- lapply(Tags,"[",nazwy)
+Tags <- lapply(Tags, changes_names,nazwy)
+Tags <- data.frame(t(data.frame(Tags,check.names = FALSE)))
+rownames(Tags) <- 1:nrow(Tags)
+
+
+Votes <- xmlToList("Votes.xml")
+len <- lapply(Votes,length)
+n <- which(unlist(len) == max(unlist(len)))[1]          
+nazwy <- names(Votes[[n]])
+Votes <- lapply(Votes,"[",nazwy)
+Votes <- lapply(Votes, changes_names,nazwy)
+Votes <- data.frame(t(data.frame(Votes,check.names = FALSE)))
+rownames(Votes) <- 1:nrow(Votes)
