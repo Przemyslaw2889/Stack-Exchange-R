6e8efcb6f92cdfe169d081950a71626ef6e213b6
unknown <d.ponikowski11@gmail.com>
2018-12-12 23:11:06 +0100

    lokalizajce miast

--- kawa.R
+++ kawa.R
@@ -1,6 +1,6 @@
 library(XML)
 options(stringsAsFactors = FALSE)
-
+setwd("coffee.stackexchange.com/")
 changes_names <- function(x,nazwy){
   names(x) <- nazwy
   x
@@ -17,17 +17,18 @@ xml_data_frame <- function(xml){
   rownames(df) <- 1:nrow(df)
   df
 }
-
 Comments <- xml_data_frame("Comments.xml")
-
 Badges <- xml_data_frame("Badges.xml")
-
 PostsHistory <- xml_data_frame("PostHistory.xml")
-
 Posts <- xml_data_frame("Posts.xml")
-
 Users <- xml_data_frame("Users.xml")
-
 Tags <- xml_data_frame("Tags.xml")
-
 Votes <- xml_data_frame("Votes.xml")
+
+library(ggmap)
+library(stringi)
+location <- strsplit(Users$Location[stri_detect_regex(Users$Location,",")], split = ",")
+location <- location[!is.na(location)]
+lokalizacje <- matrix(unlist(lapply(location[c(1:10)],function(x) geocode(x[1],source = "dsk"))),ncol = 2,byrow = TRUE)
+lokalizacje <- as.data.frame(lokalizacje)
+colnames(lokalizacje) <- c("lon","lan")
