d936db9bb187f8e2c91a25c1546771ec1ab4da72
Przemysław Kaleta <kaletap@student.mini.pw.edu.pl>
2018-12-30 15:35:00 +0100

    Dodane lokalizacje w folderze saved

--- tmp.R
+++ tmp.R
@@ -4,35 +4,36 @@ library(ggmap)
 library(stringi)
 library(maps)
 
-# Wszystkie dane mam wypakowane w folderze data
-Comments_beer <- xml_data_frame("data/beer.stackexchange.com/Comments.xml")
-Badges_beer <- xml_data_frame("data/beer.stackexchange.com/Badges.xml")
-PostsHistory_beer <- xml_data_frame("data/beer.stackexchange.com/PostHistory.xml")
-Posts_beer <- xml_data_frame("data/beer.stackexchange.com/Posts.xml")
-Users_beer <- xml_data_frame("data/beer.stackexchange.com/Users.xml")
-Tags_beer <- xml_data_frame("data/beer.stackexchange.com/Tags.xml")
-Votes_beer <- xml_data_frame("data/beer.stackexchange.com/Votes.xml")
+## Data reading
+# Wszystkie dane mam wypakowane w folderze data i przekonwertowane na csv
+Comments_beer <- read.csv("data/beer.stackexchange.com/Comments.csv")
+Badges_beer <- read.csv("data/beer.stackexchange.com/Badges.csv")
+PostsHistory_beer <- read.csv("data/beer.stackexchange.com/PostHistory.csv")
+Posts_beer <- read.csv("data/beer.stackexchange.com/Posts.csv")
+Users_beer <- read.csv("data/beer.stackexchange.com/Users.csv")
+Tags_beer <- read.csv("data/beer.stackexchange.com/Tags.csv")
+Votes_beer <- read.csv("data/beer.stackexchange.com/Votes.csv")
 
-Comments_poker <- xml_data_frame("data/poker.stackexchange.com/Comments.xml")
-Badges_poker <- xml_data_frame("data/poker.stackexchange.com/Badges.xml")
-PostsHistory_poker <- xml_data_frame("data/poker.stackexchange.com/PostHistory.xml")
-Posts_poker <- xml_data_frame("data/poker.stackexchange.com/Posts.xml")
-Users_poker <- xml_data_frame("data/poker.stackexchange.com/Users.xml")
-Tags_poker <- xml_data_frame("data/poker.stackexchange.com/Tags.xml")
-Votes_poker <- xml_data_frame("data/poker.stackexchange.com/Votes.xml")
+Comments_gaming <- read.csv("data/gaming.stackexchange.com/Comments.csv")
+Badges_gaming <- read.csv("data/gaming.stackexchange.com/Badges.csv")
+PostsHistory_gaming <- read.csv("data/gaming.stackexchange.com/PostHistory.csv")
+Posts_gaming <- read.csv("data/gaming.stackexchange.com/Posts.csv")
+Users_gaming <- read.csv("data/gaming.stackexchange.com/Users.csv")
+Tags_gaming <- read.csv("data/gaming.stackexchange.com/Tags.csv")
+Votes_gaming <- read.csv("data/gaming.stackexchange.com/Votes.csv")
 
-Comments_coffee <- xml_data_frame("data/coffee.stackexchange.com/Comments.xml")
-Badges_coffee <- xml_data_frame("data/coffee.stackexchange.com/Badges.xml")
-PostsHistory_coffee <- xml_data_frame("data/coffee.stackexchange.com/PostHistory.xml")
-Posts_coffee <- xml_data_frame("data/coffee.stackexchange.com/Posts.xml")
-Users_coffee <- xml_data_frame("data/coffee.stackexchange.com/Users.xml")
-Tags_coffee <- xml_data_frame("data/coffee.stackexchange.com/Tags.xml")
-Votes_coffee <- xml_data_frame("data/coffee.stackexchange.com/Votes.xml")
+Comments_coffee <- read.csv("data/coffee.stackexchange.com/Comments.csv")
+Badges_coffee <- read.csv("data/coffee.stackexchange.com/Badges.csv")
+PostsHistory_coffee <- read.csv("data/coffee.stackexchange.com/PostHistory.csv")
+Posts_coffee <- read.csv("data/coffee.stackexchange.com/Posts.csv")
+Users_coffee <- read.csv("data/coffee.stackexchange.com/Users.csv")
+Tags_coffee <- read.csv("data/coffee.stackexchange.com/Tags.csv")
+Votes_coffee <- read.csv("data/coffee.stackexchange.com/Votes.csv")
 
 lokacje <- function(lokalizacje){
   location <- strsplit(lokalizacje[stri_detect_regex(lokalizacje,",")], split = ",")
   location <- location[!is.na(location)]
-  lokalizacje <- matrix(unlist(lapply(location,function(x) geocode(x[1],source = "google"))),ncol = 2,byrow = TRUE)
+  lokalizacje <- matrix(unlist(lapply(location,function(x) geocode(x[1],source = "dsk"))),ncol = 2,byrow = TRUE)
   lokalizacje <- as.data.frame(lokalizacje)
   colnames(lokalizacje) <- c("lon","lan")
   lokalizacje
@@ -46,14 +47,19 @@ save_locations <- function(Users){
   print(file_path)
   if(!file.exists(file_path)){
     lokalizacje <- lokacje(Users$Location)
-    
     saveRDS(lokalizacje, file=file_path)
   } else{
     print("Locations already saved")
   }
 }
 
+save_locations(Users_coffee)
+locations_coffee <- readRDS("Users_beer_location.rds")
+
 save_locations(Users_beer)
+locations_beers <- readRDS("Users_beer_location.rds")
+
+save_locations(Users_gaming)
 locations_beers <- readRDS("Users_beer_location.rds")
 
 par(mar=c(0,0,0,0))
