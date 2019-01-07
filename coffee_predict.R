e66dd38b7c07d3e2778363194e349f58df697032
unknown <d.ponikowski11@gmail.com>
2019-01-07 18:34:16 +0100

    new model and graphics

--- coffee_predict.R
+++ coffee_predict.R
@@ -1,7 +1,6 @@
 #dane do pobrania tutaj: ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
 #source("df_from_xml.R")
 options(stringsAsFactors = FALSE)
-Posts_coffee <- read.csv("data/coffee.stackexchange.com/Posts.csv")
 #biblioteki
 library(qdap)
 library(tm)
@@ -104,35 +103,33 @@ confusionMatrix_full_model <- confusionMatrix(as.factor(test$klasa), as.factor(p
 
 
 ###FETURE EXTRACTION
-var <- apply(data_train_test_post_m, 2, var)
-data_train_test_post_m <- data_train_test_post_m[,var > 0.001]
-#rf_high_var <- randomForest(data_train_test_post_m[1:3000,], as.factor(data_train_test_post$klasa[1:3000]) )
+var <- apply(data_train_m, 2, var)
+data_train_m <- data_train_m[,var > 0.001]
+#rf_high_var <- randomForest(data_train_m, as.factor(train$klasa))
 
 #saveRDS(rf_high_var,"rf_high_var.rds")
 
 rf_high_var <- readRDS("rf_high_var.rds")
 
-pred_high_var <- predict(rf_high_var,data_train_test_post_m[3000:4000,])
-confMatr_high_var <- confusionMatrix(as.factor(data_train_test_post[3000:4000,"klasa"]), as.factor(pred_high_var))
+pred_high_var <- predict(rf_high_var,new_data_rf)
+confMatr_high_var <- confusionMatrix(as.factor(test$klasa), as.factor(pred_high_var))
 #tutaj mamy:
-#Accuracy : 0.8541, Kappa : 0.7083, Sensitivity : 0.8675, Specificity : 0.8417
-#uwzgledniajac  to ¿e zloznosc obliczeniowa jest duzo mniejsza
-#uwazam ze powinniœmy stosowaæ ten model, ale sproboje go ulepszyc, poprzez wybor zmiennych
+#Accuracy : 0.859, Kappa : 0.718, Sensitivity : 0.8472, Specificity : 0.8716
+#PYTANIE
+#KORZYSTAMY Z TEGO MODELU CZY POPRZEDNIEGO?
 
-####knn
-#knn <- knn(train = data_train_test_post_m[1:3000,],test = data_train_test_post_m[3001:4000,],cl = data_train_test_post$klasa[1:3000], k = 7)
-#saveRDS(object = knn, file = "knn_coffee.rds")
-knn_pred <- readRDS("knn_coffee.rds")
 
-confusionMatrix(as.factor(data_train_test_post[3001:4000,"klasa"]), as.factor(knn_pred))
-#accuracy 64%, kappa 27.8%, Sensitivity : 0.6894, Specificity : 0.6098
-#wiec model odrzucamy
 
 
 
 ##########################PREDYKCJA POSTOW
+#########COFFEE
+Posts_coffee <- read.csv("data/coffee.stackexchange.com/Posts.csv")
 
-pred_post_coffee <- unname(predict(rf_high_var ,newdata = data_train_test_post_m[data_train_test_post$typ == "post_coffee",]))
+post_coffee_matrix <- new_data(x = Posts_coffee$Body, names = colnames(data_train_m))
+
+
+pred_post_coffee <- unname(predict(rf_high_var ,newdata = post_coffee_matrix))
 table(pred_post_coffee)[[2]]/length(pred_post_coffee)
 
 #WYKRESY
@@ -143,27 +140,52 @@ pred_post_coffee_df$diff_time <- as.numeric(stri_extract_first_regex(pred_post_c
 pred_post_coffee_df <- pred_post_coffee_df[order(pred_post_coffee_df$creation_data), ]
 
 
-positive_by_date <- pred_post_coffee_df %>% mutate(month = stri_datetime_format(creation_data, "yyyy-MM")) %>%  group_by(month) %>% 
-  summarize(pozytywne = sum(pred == "positive"), poz_procent = sum(pred == "positive")/n(), count = n())
+positive_by_date_coffee <- pred_post_coffee_df %>% mutate(month = stri_datetime_format(creation_data, "yyyy-MM")) %>%  group_by(month) %>% 
+  summarize(positive = sum(pred == "positive"), poz_procent = sum(pred == "positive")/n(), count = n())
 
-positive_by_date_melt <- melt(data = positive_by_date[,c("count","pozytywne","month")], id.vars = "month")
+positive_coffee_by_date_melt <- melt(data = positive_by_date_coffee[,c("count","positive","month")], id.vars = "month")
 
-ggplot(positive_by_date_melt,aes(x = month, y = value, fill = variable)) + geom_bar(stat="identity",position=position_dodge())+
+ggplot(positive_coffee_by_date_melt,aes(x = month, y = value, fill = variable)) + geom_bar(stat="identity",position=position_dodge())+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+ggplot(positive_by_date_coffee,aes(x = month, y = poz_procent)) + geom_bar(stat="identity",position=position_dodge())+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#####BEER
+Posts_beer <- read.csv("data/beer.stackexchange.com/Posts.csv")
+
+post_matrix_beer <- new_data(x = Posts_beer$Body, names = colnames(data_train_m))
+
+
+pred_post_beer <- unname(predict(rf_high_var ,newdata = post_matrix_beer))
+table(pred_post_beer)[[2]]/length(pred_post_beer)
+
+#WYKRESY
+pred_post_beer_df <- data.frame(pred = pred_post_beer, creation_data = as.Date(Posts_beer$CreationDate),
+                                  last_activity_day = as.Date(Posts_beer$LastActivityDate))
+pred_post_beer_df$diff_time <- as.numeric(stri_extract_first_regex(pred_post_beer_df$last_activity_day - pred_post_beer_df$creation_data,
+                                                                     "[0-9]+"))
+pred_post_beer_df <- pred_post_beer_df[order(pred_post_beer_df$creation_data), ]
+
+
+positive_by_date_beer <- pred_post_beer_df %>% mutate(month = stri_datetime_format(creation_data, "yyyy-MM")) %>%  group_by(month) %>% 
+  summarize(positive = sum(pred == "positive"), poz_procent = sum(pred == "positive")/n(), count = n())
+
+positive_beer_by_date_melt <- melt(data = positive_by_date_beer[,c("count","positive","month")], id.vars = "month")
+
+ggplot(positive_beer_by_date_melt,aes(x = month, y = value, fill = variable)) + geom_bar(stat="identity",position=position_dodge())+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+ggplot(positive_by_date_beer,aes(x = month, y = poz_procent)) + geom_bar(stat="identity",position=position_dodge())+
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 
 
 
 
-par(mfrow = c(2,1))
-x <- barplot(as.matrix(t(positive_by_date[,c("count","pozytywne")])), las = 1,col = c("red","lightblue"), xlab = "Month",beside = TRUE,
-             main = "Posts by month", ylim = c(0,300))
-box()
-legend("topright", legend = c("All posts", "Positive"), fill = c("red","lightblue"))
-axis(1, labels = positive_by_date$month[c(1,9,18,27,36,45)], at = (x[1,c(1,9,18,27,36,45)]+x[2,c(1,9,18,27,36,45)])/2 )
 
 
-y <- barplot(positive_by_date$poz_procent, xlab = "Month", ylim = c(0,1.1),
-             col = "tan", main = "% of positive posts", las = 1)
-box()
-axis(1, labels = positive_by_date$month[c(1,9,18,27,36,45)], at = y[c(1,9,18,27,36,45),1] )
+
+
+
+
