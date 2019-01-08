fb930a92e63cc5e2e529586d7530876addecfda3
unknown <d.ponikowski11@gmail.com>
2019-01-08 16:06:16 +0100

    maps'

--- maps.R
+++ maps.R
@@ -7,29 +7,29 @@ library(dplyr)
 
 ## Data reading
 # Wszystkie dane mam wypakowane w folderze data i przekonwertowane na csv
-Comments_beer <- read.csv("data/beer.stackexchange.com/Comments.csv")
-Badges_beer <- read.csv("data/beer.stackexchange.com/Badges.csv")
-PostsHistory_beer <- read.csv("data/beer.stackexchange.com/PostHistory.csv")
+# Comments_beer <- read.csv("data/beer.stackexchange.com/Comments.csv")
+# Badges_beer <- read.csv("data/beer.stackexchange.com/Badges.csv")
+# PostsHistory_beer <- read.csv("data/beer.stackexchange.com/PostHistory.csv")
 Posts_beer <- read.csv("data/beer.stackexchange.com/Posts.csv")
-Users_beer <- read.csv("data/beer.stackexchange.com/Users.csv")
-Tags_beer <- read.csv("data/beer.stackexchange.com/Tags.csv")
-Votes_beer <- read.csv("data/beer.stackexchange.com/Votes.csv")
+#Users_beer <- read.csv("data/beer.stackexchange.com/Users.csv")
+# Tags_beer <- read.csv("data/beer.stackexchange.com/Tags.csv")
+# Votes_beer <- read.csv("data/beer.stackexchange.com/Votes.csv")
 
-Comments_gaming <- read.csv("data/gaming.stackexchange.com/Comments.csv")
-Badges_gaming <- read.csv("data/gaming.stackexchange.com/Badges.csv")
-PostsHistory_gaming <- read.csv("data/gaming.stackexchange.com/PostHistory.csv")
+# Comments_gaming <- read.csv("data/gaming.stackexchange.com/Comments.csv")
+# Badges_gaming <- read.csv("data/gaming.stackexchange.com/Badges.csv")
+# PostsHistory_gaming <- read.csv("data/gaming.stackexchange.com/PostHistory.csv")
 Posts_gaming <- read.csv("data/gaming.stackexchange.com/Posts.csv")
-Users_gaming <- read.csv("data/gaming.stackexchange.com/Users.csv")
-Tags_gaming <- read.csv("data/gaming.stackexchange.com/Tags.csv")
-Votes_gaming <- read.csv("data/gaming.stackexchange.com/Votes.csv")
+#Users_gaming <- read.csv("data/gaming.stackexchange.com/Users.csv")
+# Tags_gaming <- read.csv("data/gaming.stackexchange.com/Tags.csv")
+# Votes_gaming <- read.csv("data/gaming.stackexchange.com/Votes.csv")
 
-Comments_coffee <- read.csv("data/coffee.stackexchange.com/Comments.csv")
-Badges_coffee <- read.csv("data/coffee.stackexchange.com/Badges.csv")
-PostsHistory_coffee <- read.csv("data/coffee.stackexchange.com/PostHistory.csv")
+# Comments_coffee <- read.csv("data/coffee.stackexchange.com/Comments.csv")
+# Badges_coffee <- read.csv("data/coffee.stackexchange.com/Badges.csv")
+# PostsHistory_coffee <- read.csv("data/coffee.stackexchange.com/PostHistory.csv")
 Posts_coffee <- read.csv("data/coffee.stackexchange.com/Posts.csv")
-Users_coffee <- read.csv("data/coffee.stackexchange.com/Users.csv")
-Tags_coffee <- read.csv("data/coffee.stackexchange.com/Tags.csv")
-Votes_coffee <- read.csv("data/coffee.stackexchange.com/Votes.csv")
+#Users_coffee <- read.csv("data/coffee.stackexchange.com/Users.csv")
+# Tags_coffee <- read.csv("data/coffee.stackexchange.com/Tags.csv")
+# Votes_coffee <- read.csv("data/coffee.stackexchange.com/Votes.csv")
 
 # Comments_beer <- xml_data_frame("data/beer.stackexchange.com/Comments.xml")
 # Badges_beer <- xml_data_frame("data/beer.stackexchange.com/Badges.xml")
@@ -64,34 +64,31 @@ lokacje <- function(lokalizacje){
   lokalizacje
 }
 
-save_locations <- function(Users){
-  save_name <- deparse(substitute(Users))
-  save_name <- paste(save_name, "location", sep="_")
-  file_name <- paste(save_name, "rds", sep=".")
-  file_path <- file.path("saved", file_name)
-  print(file_path)
-  if(!file.exists(file_path)){
-    lokalizacje <- lokacje(Users$Location)
-    saveRDS(lokalizacje, file=file_path)
-  } else{
-    print("Locations already saved")
-  }
-}
+# save_locations <- function(Users){
+#   save_name <- deparse(substitute(Users))
+#   save_name <- paste(save_name, "location", sep="_")
+#   file_name <- paste(save_name, "rds", sep=".")
+#   file_path <- file.path("saved", file_name)
+#   print(file_path)
+#   if(!file.exists(file_path)){
+#     lokalizacje <- lokacje(Users$Location)
+#     saveRDS(lokalizacje, file=file_path)
+#   } else{
+#     print("Locations already saved")
+#   }
+# }
 
-save_locations(Users_coffee)
+# save_locations(Users_coffee)
 locations_coffee <- readRDS(file.path("saved", "Users_coffee_location.rds"))
 
-save_locations(Users_beer)
+# save_locations(Users_beer)
 locations_beers <- readRDS(file.path("saved", "Users_beer_location.rds"))
 
-save_locations(Users_gaming)
+# save_locations(Users_gaming)
 locations_gaming <- readRDS(file.path("saved", "Users_gaming_location.rds"))
 
-##### BEER
-par(mar=c(0,0,0,0))
-map('world',col="#f2f2f2", fill=TRUE, bg="white", lwd=0.05,mar=rep(0,4),border=0, ylim=c(-80,80),main ="mapa")
-points(locations_beers$lon,locations_beers$lan,pch = 16,cex = 0.7, col = rgb(red = 0, green = 0, blue = 0, alpha = 0.2))
 
+##### BEER
 najaktywniejsi_uzytkownicy_beer<- Posts_beer %>% group_by(OwnerUserId) %>% summarise(ilosc = n()) %>% 
   left_join(Users_beer,by = c("OwnerUserId" = "Id")) %>% select(ilosc,OwnerUserId, Location) %>% arrange(desc(ilosc)) %>% as.data.frame()
 
@@ -104,16 +101,8 @@ najw_pkt_beer <- Posts_beer %>% group_by(OwnerUserId) %>% summarise(pkt = sum(as
 
 lok_najw_pkt_beer <- lokacje(najw_pkt_beer$Location[1:10])
 
-points(lok_najw_pkt_beer$lon,lok_najw_pkt_beer$lan, pch = 16, cex = 0.7, col = "blue")
-
-legend(x = -183, y = -53,col = c("red","blue"), legend = c("most active users","users with highest number of likes"),pch = 16)
-
-title("Beer")
 
 ##### COFFEE
-par(mar=c(0,0,0,0))
-map('world',col="#f2f2f2", fill=TRUE, bg="white", lwd=0.05,mar=rep(0,4),border=0, ylim=c(-80,80),main ="mapa")
-points(locations_coffee$lon,locations_coffee$lan,pch = 16,cex = 0.7, col = rgb(red = 0, green = 0, blue = 0, alpha = 0.2))
 
 najaktywniejsi_uzytkownicy <- Posts_coffee %>% group_by(OwnerUserId) %>% summarise(ilosc = n()) %>% 
   left_join(Users_coffee,by = c("OwnerUserId" = "Id")) %>% select(ilosc,OwnerUserId, Location) %>% arrange(desc(ilosc)) %>% as.data.frame()
@@ -127,24 +116,15 @@ najw_pkt <- Posts_coffee %>% group_by(OwnerUserId) %>% summarise(pkt = sum(as.nu
 
 lok_najw_pkt <- lokacje(najw_pkt$Location[1:10])
 
-points(lok_najw_pkt$lon,lok_najw_pkt$lan, pch = 16, cex = 0.7, col = "blue")
-
-legend(x = -183, y = -53,col = c("red","blue"), legend = c("most active users","users with highest number of likes"),pch = 16)
-title("Coffee")
-
-#Ranking miast
-miasta_najwieksza_ilosc_pkt <- najw_pkt %>% group_by(Location) %>% summarise(suma = sum(pkt)) %>% arrange(desc(suma)) %>% na.omit()
-miasta_najaktywniejsi_uzytkownicy <- najaktywniejsi_uzytkownicy %>% group_by(Location) %>% summarise(suma = sum(ilosc)) %>%
-  arrange(desc(suma)) %>% na.omit()
-miasta_najaktywniejsi_uzytkownicy[1:10,]
-miasta_najwieksza_ilosc_pkt[1:10,]
+# #Ranking miast
+# miasta_najwieksza_ilosc_pkt <- najw_pkt %>% group_by(Location) %>% summarise(suma = sum(pkt)) %>% arrange(desc(suma)) %>% na.omit()
+# miasta_najaktywniejsi_uzytkownicy <- najaktywniejsi_uzytkownicy %>% group_by(Location) %>% summarise(suma = sum(ilosc)) %>%
+#   arrange(desc(suma)) %>% na.omit()
+# miasta_najaktywniejsi_uzytkownicy[1:10,]
+# miasta_najwieksza_ilosc_pkt[1:10,]
 
 
 ##### GAMING
-par(mar=c(0,0,0,0))
-map('world',col="#f2f2f2", fill=TRUE, bg="white", lwd=0.05,mar=rep(0,4),border=0, ylim=c(-80,80),main ="mapa")
-points(locations_gaming$lon,locations_gaming$lan,pch = 16,cex = 0.7, col = rgb(red = 0, green = 0, blue = 0, alpha = 0.05))
-
 
 najaktywniejsi_uzytkownicy_gam <- Posts_gaming %>% group_by(OwnerUserId) %>% summarise(ilosc = n()) %>% 
   left_join(Users_coffee,by = c("OwnerUserId" = "Id")) %>% select(ilosc,OwnerUserId, Location) %>% arrange(desc(ilosc)) %>% as.data.frame()
@@ -158,9 +138,20 @@ najw_pkt_gam <- Posts_gaming %>% group_by(OwnerUserId) %>% summarise(pkt = sum(a
 
 lok_najw_pkt_gam <- lokacje(najw_pkt_gam$Location[1:10])
 
-points(lok_najw_pkt$lon,lok_najw_pkt$lan, pch = 16, cex = 0.7, col = "blue")
+#funkcja do aplikacji
 
-legend(x = -183, y = -53,col = c("red","blue"), legend = c("most active users","users with highest number of likes"),pch = 16)
-title("Gaming")
 
+lista_location <- list(coffee = locations_coffee,beer = locations_beers,gaming = locations_gaming)
+lista_najw_pkt <- list(coffee = lok_najw_pkt, beer = lok_najw_pkt_beer, gaming = lok_najw_pkt_gam )
+lista_najaktyw <- list(beer = lok_naj_aktyw_beer, coffee = lok_naj_aktyw, gaming = lok_naj_aktyw_gam )
+
+mapa <- function(forum){
+  par(mar=c(0,0,0,0))
+  map('world',col="#f2f2f2", fill=TRUE, bg="white", lwd=0.05,mar=rep(0,4),border=0, ylim=c(-80,80),main ="mapa")
+  points(lista_location[[forum]]$lon,lista_location[[forum]]$lan,pch = 16,cex = 0.7, col = rgb(red = 0, green = 0, blue = 0, alpha = 0.2))
+  title(forum)
+  points(lista_najw_pkt[[forum]]$lon, lista_najw_pkt[[forum]]$lan,pch=16,cex = 0.7,col = "blue")
+  points(lista_najaktyw[[forum]]$lon , lista_najaktyw[[forum]]$lan,pch=16,cex = 0.7, col = "red")
+  legend(x = -183, y = -53,col = c("red","blue"), legend = c("most active users","users with highest number of likes"),pch = 16)
+}
 
