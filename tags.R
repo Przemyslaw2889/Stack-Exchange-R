dc341f3bb6ff1629d80167fce2ced53b41bcafef
unknown <d.ponikowski11@gmail.com>
2019-01-11 14:58:01 +0100

    ggplot2 tags

--- tags.R
+++ tags.R
@@ -10,18 +10,22 @@ tags_barplot <- function(Tags, k=10, ...){
   # Przejrzymy najpierw jakie sa popularne tagi
   popular_tags <- Tags%>% 
     arrange(desc(Count)) %>% head(k)
+  #print(popular_tags)
   # Barplot tagow
   counts <- table(popular_tags$Count)
-  opt <- par(mai=c(1,2.5,1,1))
-  barplot(popular_tags$Count, names.arg=popular_tags$TagName, las=2, horiz=TRUE, ...)
-}
+  #print(counts)
+  #opt <- par(mai=c(1,2.5,1,1))
+  #barplot(popular_tags$Count, names.arg=popular_tags$TagName, las=2, horiz=TRUE, ...)
+  ggplot(popular_tags,aes(x = reorder(TagName,Count), y = Count))+ geom_bar(stat="identity",position='dodge') +
+     coord_flip() + xlab("TagName") + labs(title = "Popular Tags")
+  }
 
 # tags_barplot(Tags_beer)
 # tags_barplot(Tags_gaming, 10)
 # tags_barplot(Tags_coffee, 5)
 
-tags_barplot_name <- function(name, k=10, ...){
-  tags = list(coffee=Tags_coffee, beer=Tags_beer, gaming=Tags_gaming)
-  tags_barplot(tags[[name]], k, ...)
-}
+# tags_barplot_name <- function(name, k=10, ...){
+#   tags = list(coffee=Tags_coffee, beer=Tags_beer, gaming=Tags_gaming)
+#   tags_barplot(tags[[name]], k, ...)
+# }
 
