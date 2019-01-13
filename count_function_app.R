e4ca5a91c95c25943097b2cad7f6578df0bac0e8
unknown <d.ponikowski11@gmail.com>
2019-01-13 15:05:31 +0100

    change plot type

--- count_function_app.R
+++ count_function_app.R
@@ -1,3 +1,6 @@
+library(dplyr)
+library(ggplot2)
+library(stringi)
 #COFFEE
 # Votes_coffee <- read.csv("data/coffee.stackexchange.com/Votes.csv")
 Comments_coffee <- read.csv("data/coffee.stackexchange.com/Comments.csv")
@@ -61,15 +64,16 @@ list_of_count <- list(gaming = gaming_df, beer = beer_df, coffee = coffee_df)
 
 count_post_comment <- function(forum){
   if (forum == "gaming"){
-  ggplot(list_of_count[[forum]], aes(x = date,y = liczba,fill = typ)) + geom_bar(stat="identity",position='dodge') +
+  ggplot(list_of_count[[forum]], aes(x = date,y = liczba,colour = typ, group = typ)) + geom_point() + geom_line() +
     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +  scale_x_discrete(breaks = unique(gaming_df$date)[
       seq(1, length(unique(gaming_df$date)), by = 3)]) + labs(title = paste("Number of posts and comments on",forum,"forum"))
     }
   else{
-    ggplot(list_of_count[[forum]], aes(x = date,y = liczba,fill = typ)) + geom_bar(stat="identity",position='dodge') +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +  scale_x_discrete(breaks = unique(list_of_count[[forum]]$date)[
+    ggplot(list_of_count[[forum]], aes(x = date,y = liczba,group = typ, colour = typ))  + geom_point(size = 2) + geom_line() +
+     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +  scale_x_discrete(breaks = unique(list_of_count[[forum]]$date)[
         seq(1, length(unique(list_of_count[[forum]]$date)), by = 2)]) + labs(title = paste("Number of posts and comments on",forum,"forum"))
   }
 }
 
-count_post_comment("coffee")
+#count_post_comment("gaming")
+
